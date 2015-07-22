2f9520e6986d40af06c50932f2c08bfc1820a557
gmltA <gmltt@yandex.ru>
2015-07-22 13:03:50 +0300

    Login, register, main page drafts

---
+++ KPaTS/Core/PageID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KPaTS.Core
+{
+    public enum PageID
+    {
+        PAGE_INDEX,
+        PAGE_AUTH
+    }
+}
