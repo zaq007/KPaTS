aa35289f7dd30696c4faca93c818ef8277603562
gmltA <gmltt@yandex.ru>
2015-11-15 20:38:12 +0300

    Test passing remade

--- KPaTS/Scripts/scripts.js
+++ KPaTS/Scripts/scripts.js
@@ -10,3 +10,26 @@
 
     return (((elemTop >= docViewTop) && (elemTop <= docViewBottom)) || ((elemBottom >= docViewTop) && (elemBottom <= docViewBottom)));
 }
+
+function registerScrollTransformation(view, viewTarget, button, origin, destination) {
+    $(view).scroll(function () {
+        if (!isScrolledIntoView($(viewTarget)) && !$("#test-fab").hasClass("header")) {
+            $(button).addClass("transition");
+            setTimeout(function () {
+                $(button).appendTo($(destination)).addClass("header");
+                setTimeout(function () {
+                    $(button).removeClass("transition");
+                }, 200);
+            }, 200);
+        }
+        else if (isScrolledIntoView($(viewTarget)) && $(button).hasClass("header")) {
+            $(button).addClass("transition");
+            setTimeout(function () {
+                $(button).appendTo($(origin)).removeClass("header");
+                setTimeout(function () {
+                    $(button).removeClass("transition");
+                }, 200);
+            }, 200);
+        }
+    });
+}
