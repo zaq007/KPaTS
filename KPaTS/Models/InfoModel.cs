e0008d07f223409dbba14602383a585889ce22b0
gmlt.A <gmltt@yandex.ru>
2015-11-17 12:40:53 +0300

    Theory attachment front-end draft

--- KPaTS/Models/InfoModel.cs
+++ KPaTS/Models/InfoModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace KPaTS.Models
 {
@@ -15,6 +16,10 @@ namespace KPaTS.Models
 
         [Required]
         public string Name { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Text { get; set; }
 
         [Required]
         public string Link { get; set; }
