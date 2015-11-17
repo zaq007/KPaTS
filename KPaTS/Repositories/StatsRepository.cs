400bc3aed6f100da1691e429f2384c36c308b1f2
zaq007 <sergey4000@tut.by>
2015-11-17 11:37:42 +0300

    GetTestAtats method added.

--- KPaTS/Repositories/StatsRepository.cs
+++ KPaTS/Repositories/StatsRepository.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using KPaTS.Models;
 
 namespace KPaTS.Repositories
 {
     public class StatsRepository
     {
+
+        public List<StatModel> GetTestStats(Guid id)
+        {
+            using (var DB = new MainContext())
+            {
+                return DB.Stats.Where(x => x.TestModel.Id == id).ToList();
+            }
+        }
+
     }
 }
