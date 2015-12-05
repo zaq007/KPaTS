896c4d1d3900249ff72c8e5124bc5e419d489642
zaq007 <sergey4000@tut.by>
2015-12-05 21:40:21 +0300

    View-all

--- KPaTS/Controllers/HomeController.cs
+++ KPaTS/Controllers/HomeController.cs
@@ -35,5 +35,22 @@ namespace KPaTS.Controllers
             List<AutocompleteInfo> spaces = new SpacesRepository().GetAutocompleteItems(data.space, data.test);
             return PartialView(spaces.Union(tests).ToList());
         }
+
+        public ActionResult Search(string query)
+        {
+            dynamic data = AutocompleteAlgorithms.ParseQuery(query);
+            if (string.Compare(data.test, "") != 0 && string.Compare(data.space, "") == 0)
+                ViewBag.Tests = new TestsRepository().GetTestsForAutocomplete(data.space, data.test);
+            else
+                if (string.Compare(data.space, "") != 0 && string.Compare(data.test, "") == 0)
+                    ViewBag.Spaces = new SpacesRepository().GetAutocompleteItems(data.space, data.test);
+                else
+                {
+                    var Tests = new TestsRepository().GetTestsForAutocomplete(data.space, data.test);
+                    if (Tests.Count() == 1)
+                        RedirectToAction("Index", "Test", new { Id = Tests.First().Id });
+                }
+            return View();
+        }
     }
 }
